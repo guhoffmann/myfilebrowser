11896b4635996b8fbfc6e7ada38a5a57720ea6f2
guhoffmann <gert.uwe.hoffmann@gmail.com>
2018-11-01 12:41:52 +0100

    Changed JavaScript

--- js/router.js
+++ js/router.js
@@ -10,46 +10,46 @@ const startingPoint = document.getElementById('app');
 
 const myrouter = function () {
   
-   // object containing all possible routes
-   
+	// object containing all possible routes
+
 	var routes = {};
-   
-   function handleRouting() {
-      defaultRoute = '*';
-      currentHash = location.hash.slice(1); // cut hash from location
+
+	function handleRouting() {
+		defaultRoute = '*';
+		currentHash = location.hash.slice(1); // cut hash from location
 
 		if ( routes.hasOwnProperty(currentHash) ) {
 			routeHandler = routes[currentHash];
 		} else { 
 			routeHandler = routes[defaultRoute];
 		}
-      // if route handler is found, call it!
+		// if route handler is found, call it!
 
-      if (routeHandler) {
-         routeHandler();
-      }
-   };
-   
-  // ON INITIALISATION:
-  // call route handling on events 'hashchange', 'load'
-  
-   if (window) {
-      window.addEventListener('hashchange', handleRouting);
-      window.addEventListener('load', handleRouting);
-   }
-   
-   // return object containing adRoute, navigateToHashUrl
+		if (routeHandler) {
+			routeHandler();
+		}
+	};
+
+	// ON INITIALISATION:
+	// call route handling on events 'hashchange', 'load'
+
+	if (window) {
+		window.addEventListener('hashchange', handleRouting);
+		window.addEventListener('load', handleRouting);
+	}
+
+	// return object containing adRoute, navigateToHashUrl
 
 	return {
 		addRoute: function (hashUrl, routeHandler) {
 			routes[hashUrl] = routeHandler;
-   		}
+			}
 		,
 		navigateToHashUrl: function (hashUrl) {
-      		location.hash = hashUrl;
-   		}
+				location.hash = hashUrl;
+			}
 	};
-   
+
 }(); // create and do the self-execution!
 
 /** add routes and event handlers **********************************************
@@ -57,29 +57,30 @@ const myrouter = function () {
 
 myrouter.addRoute('list', function () {
    
-   path = window.location.search.substr(1);
-   globalAktMediaPath = path;
-   
-   $.ajax({
-            url: "cgi-bin/dirlist.php",
-            data: { pathname: path },
-            dataType: "text html", // must be sent for browser to get response correctly!
-            success: function(data){
-                startingPoint.innerHTML = data;
-            }
-    });
-});
+	path = window.location.search.substr(1);
+	globalAktMediaPath = path;
+
+	$.ajax({
+				url: "cgi-bin/dirlist.php",
+				data: { pathname: path },
+				dataType: "text html", // must be sent for browser to get response correctly!
+				success: function(data){
+					 startingPoint.innerHTML = data;
+				}
+	 });
+
+}); // of myrouter.addRoute('list'...
 
 myrouter.addRoute('*', function () {
    
-   $.ajax({
-            url: "cgi-bin/dirlist.php",
-            data: { pathname: '/' },
-            dataType: "text html", // must be sent for browser to get response correctly!
-            success: function(data){
-                startingPoint.innerHTML = data;
-            }
-    });
-});
+	$.ajax({
+		url: "cgi-bin/dirlist.php",
+		data: { pathname: '/' },
+		dataType: "text html", // must be sent for browser to get response correctly!
+		success: function(data){
+			 startingPoint.innerHTML = data;
+		}
+	});
 
+}); // of myrouter.addRoute
 
