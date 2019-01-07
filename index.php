42e97c84665d4c9f344a276947a147b86a491bb1
uwe <gert.uwe.hoffmann@gmail.com>
2019-01-07 12:34:22 +0100

    Modularized index.php for clearness

--- index.php
+++ index.php
@@ -15,25 +15,19 @@ if ( !isset($_SESSION["language"]) ) {
 	$_SESSION["language"] = "1";
 }
 
-// Necessary to keep clipboard alive:
-// only clear if not existing!!!
-// 'clipboard' must be named 'memory' to not be confounded
-// with the language string 'clipboard'!!!
+/* Necessary to keep clipboard alive: only clear if not existing!!!
+ 'clipboard' is named 'memory' to not be confused with language string 'clipboard'! */
 if ( !isset($_SESSION["memory"]) ) { 
 	$_SESSION["memory"] = array();
 }
 
-include('cgi-bin/myFunctions.php');
+include 'cgi-bin/myFunctions.php';
 
 // Now fetch all language dependent Strings for menu and main page!
-
 $db = connect_db();
 $result = $db->query('SELECT name,value FROM strings WHERE language = '.$_SESSION["language"]);
-
 while ($row = $result->fetch()) {
-	
 	$_SESSION[$row[0]] = $row[1];
-
 }
 
 echo '
@@ -54,52 +48,13 @@ echo '
 	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
 	<meta name="msapplication-TileColor" content="#da532c">
 	<meta name="theme-color" content="#ffffff">
-
 </head>
+<body>';
 
-<body>
+// include windows section
+include 'modalWindow.php';
 
-	<!-- ******************************************************* -->
-	<!-- Bootstrap Modal Window used for all messages in the app -->
-
-	<div class="modal fade" id="ModalMessage" tabindex="10" role="dialog" aria-labelledby="ModalTitle" aria-hidden="true">
-		<div class="modal-dialog modal-dialog-centered" role="document">
-			<div class="modal-content">
-
-				<div class="modal-header">
-					<h5 class="modal-title" id="ModalTitle">Nachricht</h5>
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true">&times;</span>
-					</button>
-				</div> <!-- modal-header -->
-
-				<!-- section for value input -->
-				<input class="hidden" type="text" id="inputval" name="foldername" value="">
-
-				<!-- section for the upload -->
-				<form id="upload" action="cgi-bin/actions.php" method="post" enctype="multipart/form-data"> 
-					<input type="file" name="file[]" id="fileinput" class="inputfile" data-multiple-caption="{count} Dateien hochladen" multiple="multiple"/>
-               <!-- label MUST follow file input immediately to work! -->
-               <label id="filebuttlabel" class="filebuttlabel" for="fileinput">Datei(en) ausw&auml;hlen...</label>
-               <!-- hidden value for upload directory -->
-               <input id="uploadDir" type="hidden" value="path to upload" name="uploadDir" />
-					<input id="action" type="hidden" value="uploadPost" name="action" />
-
-				</form>
-
-				<!-- section for modal window content -->
-				<div class="modal-body" id="ModalContent">
-				</div>
-
-				<!-- section for the modal window footer -->
-				<div class="modal-footer">
-					<button type="button" id="ModalOk" class="material-icons btn btn-primary" data-dismiss="modal">done</button>
-					<button type="button" id="ModalClose" class="material-icons btn btn-primary" data-dismiss="modal">clear</button>
-				</div>
-				
-			</div>
-		</div>
-	</div>
+echo '
 
 	<!-- ******************************************************* -->
 	<!--          The navbar for the whole HTML app              -->
---
+++ modalWindow.php
@@ -0,0 +1,42 @@
+<!-- ******************************************************* -->
+<!-- Bootstrap Modal Window used for all messages in the app -->
+
+<div class="modal fade" id="ModalMessage" tabindex="10" role="dialog" aria-labelledby="ModalTitle" aria-hidden="true">
+	<div class="modal-dialog modal-dialog-centered" role="document">
+		<div class="modal-content">
+
+			<div class="modal-header">
+				<h5 class="modal-title" id="ModalTitle">Nachricht</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div> <!-- modal-header -->
+
+			<!-- section for value input -->
+			<input class="hidden" type="text" id="inputval" name="foldername" value="">
+
+			<!-- section for the upload -->
+			<form id="upload" action="cgi-bin/actions.php" method="post" enctype="multipart/form-data"> 
+				<input type="file" name="file[]" id="fileinput" class="inputfile" data-multiple-caption="{count} Dateien hochladen" multiple="multiple"/>
+				<!-- label MUST follow file input immediately to work! -->
+				<label id="filebuttlabel" class="filebuttlabel" for="fileinput">Datei(en) ausw&auml;hlen...</label>
+				<!-- hidden value for upload directory -->
+				<input id="uploadDir" type="hidden" value="path to upload" name="uploadDir" />
+				<input id="action" type="hidden" value="uploadPost" name="action" />
+
+			</form>
+
+			<!-- section for modal window content -->
+			<div class="modal-body" id="ModalContent">
+			</div>
+
+			<!-- section for the modal window footer -->
+			<div class="modal-footer">
+				<button type="button" id="ModalOk" class="material-icons btn btn-primary" data-dismiss="modal">done</button>
+				<button type="button" id="ModalClose" class="material-icons btn btn-primary" data-dismiss="modal">clear</button>
+			</div>
+			
+		</div> <!-- modal content -->
+	</div> <!-- class="modal-dialog modal-dialog-centered" role="document" -->
+</div> <!-- class="modal fade" ... -->
+
