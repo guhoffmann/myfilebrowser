23bb8f5b11b10b5e3950b170ea6732af985eec91
guhoffmann <gert.uwe.hoffmann@gmail.com>
2019-02-15 14:40:14 +0100

    Added some info in nginx.conf

--- start-nginx.sh
+++ start-nginx.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-# Start the HTTP file server with nginx nginx-local.conf file in conf directory!
+# Start the HTTP file server with nginx.conf file in conf directory!
 # The php-fpm module has to be started before!
 
+# Please notice: nginx is started as user www-data normally!
+# So the root directory of your docs folder should have
+# read/write access for this user!
 sudo systemctl start php7.0-fpm.service
-sudo /usr/sbin/nginx -c $(pwd)/conf/nginx-local.conf
+sudo /usr/sbin/nginx -c $(pwd)/conf/nginx.conf
 
