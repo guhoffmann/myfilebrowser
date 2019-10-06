873367a583874ecfe4299330810b8defcde78d1b
guhoffmann <gert.uwe.hoffmann@gmail.com>
2019-10-06 19:51:27 +0200

    Changed password.sh to generate passwords

--- password.sh
+++ password.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-# create password hash
+# create password hash from string parameter given
 
+if [ "$#" -ne 1 ];then
+	echo
+	echo "password.sh"
+	echo
+	echo "Compute hash value for the string given as parameter."
+	echo "Suited to generate passwords for use with PHP password_verify(..)."
+	echo
+	exit 1
+fi
 PW=$1
 hash=$(php -r "print password_hash('$PW',PASSWORD_DEFAULT);")
 echo $hash
