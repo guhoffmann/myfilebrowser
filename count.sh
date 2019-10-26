b4ba5300e8ecf9b6c8c6388f5245610e37098bd7
guhoffmann <gert.uwe.hoffmann@gmail.com>
2019-10-26 22:26:31 +0200

    changed count.sh

--- count.sh
+++ count.sh
@@ -2,7 +2,7 @@
 # count lines of code written for this project ;)
 
 c=0
-arr="*.php cgi-bin/*.php js/router.js js/myFunctions.js css/custom.css *.sh conf/*.*"
+arr="*.php cgi-bin/*.* js/router.js js/myFunctions.js css/custom.css *.sh conf/*.* admin/*.*"
 for i in $arr;do
 	c=$(expr  $c + $(wc -l $i|awk '{print $1}'))
 done
